389dbd6ede42037a809722cb3a65023e1a4347e5
udzhin229 <mijek200311@gmail.com>
2024-10-19 01:44:36 +0300

    four block img rotate

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,4 +1,7 @@
 import { useTranslation } from 'react-i18next';
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import './style.scss';
 import headerVector from './header.png';
@@ -6,16 +9,33 @@ import Service from '../service/Service';
 
 import { servicesList } from './../../data/servicesList';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Services = () => {
     const { t, i18n } = useTranslation();
     const currentLanguageKey = i18n.language;
+    const ImgRotate = useRef();
+    const services = useRef();
+
+    useEffect(() => {
+         gsap.to(ImgRotate.current, {
+            rotate: 360,
+            ease: 'power2.out',
+            duration: 1.5, // Продолжительность анимации в секундах
+            scrollTrigger: {
+                trigger: services.current,
+                start: 'top+=50 center', // Запуск анимации, когда блок services на 100px ниже
+                markers: true
+            },
+        });
+    }, []);
 
     return (
-        <section className="services not-sticky">
+        <section ref={services} className="services not-sticky">
             <div className="container">
                 <div className="services__header">
                     {t('services')}
-                    <img src={headerVector} alt="" />
+                    <img ref={ImgRotate} src={headerVector} alt="" />
                 </div>
                 <div className="services__wrapper">
                     {servicesList.map((service) => {
